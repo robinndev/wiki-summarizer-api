3de315cc6b1918ee1606b6c61c13339f6c338868
Robson da Silva Filho <robson.filho.ext@einstein.br>
2025-12-29 01:32:27 -0300

    feat: add docker and docker compose

--- app/db/session.py
+++ app/db/session.py
@@ -5,7 +5,12 @@ from app.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL, 
+    pool_size=10, 
+    max_overflow=20,
+    pool_pre_ping=True,
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.db.session import engine  # importa seu engine do SQLAlchemy
 from app.api.routes.health import router as health_router
 from app.api.routes.summaries import router as summaries_router
 
@@ -9,3 +11,13 @@ app = FastAPI(title="Wikipedia Summarizer LLM API")
 
 app.include_router(health_router)
 app.include_router(summaries_router)
+
+@app.on_event("startup")
+def startup_check():
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        print("Database connection OK")
+    except Exception as e:
+        print("Database connection failed")
+        raise e
